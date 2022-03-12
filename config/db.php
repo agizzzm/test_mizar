93f450fe94d5b7b725329b6dc9282fc2acec67d7
Amal <Izmailov.A@citilink.ru>
2022-03-12 16:35:45 +0300

    DB configs fixed

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=database;dbname=mizar',
+    'username' => 'mizar',
+    'password' => 'secret',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
