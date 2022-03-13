1119f8d83bc3b9e9cd1d8e594cc0ca11476c1d07
Amal <Izmailov.A@citilink.ru>
2022-03-13 10:17:29 +0300

    StringHelper created

--- models/helpers/StringHelper.php
+++ models/helpers/StringHelper.php
@@ -1,1 +1,11 @@
 <?php
+
+namespace app\models\helpers;
+
+class StringHelper
+{
+    public static function filterString(string $string)
+    {
+        return strip_tags(addslashes($string));
+    }
+}
